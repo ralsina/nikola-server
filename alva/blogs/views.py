f1851d0282daf85d838e0706da9635ec63afdee4
Roberto Alsina <ralsina@netmanagers.com.ar>
2013-05-22 12:08:45 -0300

    require login

--- alva/blogs/views.py
+++ alva/blogs/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
 
-
+@login_required
 def home(request):
     return HttpResponse("Home")
