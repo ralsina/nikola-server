ebd539c715fbb81ad358750faf72c9ffeb69c91d
Roberto Alsina <ralsina@netmanagers.com.ar>
2013-05-22 13:19:42 -0300

    full models

--- alva/blogs/models.py
+++ alva/blogs/models.py
@@ -1,12 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+LANGUAGE_CHOICES = (
+    ('en', 'English'),
+    ('es', 'Spanish'),
+)
 
 # Create your models here.
 
 class Blog(models.Model):
-    pass
+    owner = models.ForeignKey(User, related_name="owner_of")
+    members = models.ManyToManyField(User, related_name="member_of")
+    name = models.CharField(max_length=64, unique=True)
+    domain = models.CharField(max_length=64, unique=True)
+    description = models.TextField(max_length=500, default="")
+    language = models.CharField(max_length=9, choices=LANGUAGE_CHOICES)
 
 class Post(models.Model):
-    pass
+    author = models.ForeignKey(User)
+    blogs = models.ManyToManyField(Blog)
+    title = models.CharField(max_length=128)
+    slug = models.CharField(max_length=128)
+    text = models.TextField(max_length=100000)
+    date = models.DateTimeField(auto_now_add=True)
+    tags = models.CharField(max_length=512)
+    description = models.TextField(max_length=1024)
 
-class Story(models.Model):
+class Story(Post):
     pass
