1f2b9e5bc4fcd8f4be2ed0ff782512b3e63a1661
Roberto Alsina <ralsina@netmanagers.com.ar>
2013-05-22 13:19:32 -0300

    put more stuff in admin

--- alva/blogs/admin.py
+++ alva/blogs/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from blogs.models import Blog
+from blogs.models import Blog, Post, Story
 
 admin.site.register(Blog)
+admin.site.register(Post)
+admin.site.register(Story)
