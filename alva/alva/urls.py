3301a34a356a0dcebb056c1f233e1bbdad460db1
Roberto Alsina <ralsina@netmanagers.com.ar>
2013-05-22 11:43:30 -0300

    dummy home view

--- alva/alva/urls.py
+++ alva/alva/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'alva.views.home', name='home'),
+    url(r'^$', 'blogs.views.home', name='home'),
     # url(r'^alva/', include('alva.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
--- alva/blogs/views.py
+++ alva/blogs/views.py
@@ -1,1 +1,7 @@
+from django.http import HttpResponse
+
 # Create your views here.
+
+
+def home(request):
+    return HttpResponse("Home")
