34d26a0fc2df193fbc3d29766f8f039620a738cb
Roberto Alsina <ralsina@netmanagers.com.ar>
2013-05-27 14:27:12 -0300

    form tweaks

--- alva/blogs/forms.py
+++ alva/blogs/forms.py
@@ -1,16 +1,28 @@
 # forms.py
 from django import forms
+
+from bootstrap_toolkit.widgets import BootstrapTextInput, BootstrapDateInput
+
 from blogs.models import Blog, Post, Story
+
+
+class NameWidget(BootstrapTextInput):
+    def __init__(self, *a, **k):
+        k['append'] = '.donewithniko.la'
+        super(NameWidget, self).__init__(*a, **k)
 
 class BlogForm(forms.ModelForm):
     class Meta:
         model = Blog
-        exclude = ('owner', 'dirty', 'members')
+        exclude = ('owner', 'dirty', 'members', 'domain')
+        widgets = {
+            'name': NameWidget,
+        }
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        exclude = ('author',)
+        exclude = ('author', 'blogs')
 
 class StoryForm(forms.ModelForm):
     class Meta:
