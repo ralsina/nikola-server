f527099ea868be15347937666e8aa7c266e76072
Roberto Alsina <ralsina@netmanagers.com.ar>
2013-05-27 12:24:52 -0300

    added south, used absolute path for db

--- alva/alva/settings.py
+++ alva/alva/settings.py
@@ -16,7 +16,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'alva.sql',                      # Or path to database file if using sqlite3.
+        'NAME': os.path.join(PROJECT_ROOT, 'alva.sql'),                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -139,6 +139,7 @@ INSTALLED_APPS = (
     'django_rq',
     'bootstrap_toolkit',
     'debug_toolbar',
+    'south',
 
     'blogs',
 
