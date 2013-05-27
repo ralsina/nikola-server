710bde5e17bc403dd856e864c340300c48c2609e
Roberto Alsina <ralsina@netmanagers.com.ar>
2013-05-27 13:07:03 -0300

    default to english

--- alva/blogs/models.py
+++ alva/blogs/models.py
@@ -25,7 +25,7 @@ class Blog(models.Model):
     domain = models.CharField(max_length=64, blank=True)
     title = models.CharField(max_length=128, unique=True)
     description = models.TextField(max_length=500, blank=True)
-    language = models.CharField(max_length=9, choices=LANGUAGE_CHOICES)
+    language = models.CharField(max_length=9, choices=LANGUAGE_CHOICES, default='en')
     dirty = models.BooleanField(default=False)
 
     def path(self):
