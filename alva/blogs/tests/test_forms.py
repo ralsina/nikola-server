5a2610370456ef97c3a88494032ab5c779afa12d
juacompe <juacompe@gmail.com>
2013-08-01 11:51:53 +0700

    - add test for creating post & story on 13th Jan

--- alva/blogs/tests/test_forms.py
+++ alva/blogs/tests/test_forms.py
@@ -6,6 +6,18 @@ from blogs.tests import factory
 
 
 class TestPostForm(TestCase):
+    data = {
+        'blog': 1,
+        'date_0': '01/08/2013 09:20',
+        'description': '',
+        'dirty': True,
+        'markup': 'markdown',
+        'slug': 'my-first-blog',
+        'tags': 'life style, diary',
+        'text': 'Papoy? Wha kind a papoy? No no no, paPOY! Ohhh, paPOY. He he he',
+        'title': 'My first blog'
+    }
+
     def setUp(self):
         factory.create_blog()
 
@@ -17,22 +29,29 @@ class TestPostForm(TestCase):
         self.assertFalse(form.is_valid())
 
     def test_create_valid_post(self):
-        data = {
-            'blog': 1,
-            'date_0': '01/08/2013 09:20',
-            'description': '',
-            'dirty': True,
-            'markup': 'markdown',
-            'slug': 'my-first-blog',
-            'tags': 'life style, diary',
-            'text': 'Papoy? Wha kind a papoy? No no no, paPOY! Ohhh, paPOY. He he he',
-            'title': 'My first blog'
-        }
-        form = PostForm(data=data)
+        form = PostForm(data=self.data)
         self.assertTrue(form.is_valid())
+
+    def test_create_post_on_13th_jan(self):
+        data = self.data.copy()
+        data['date_0'] = '13/01/2013 00:16'
+        form = PostForm(data=self.data)
+        self.assertTrue(form.is_valid(), form.errors)
 
 
 class TestPostStory(TestCase):
+    data = {
+        'blog': 1,
+        'date': '01/08/2013 09:20',
+        'description': '',
+        'dirty': True,
+        'markup': 'markdown',
+        'slug': 'my-first-blog',
+        'tags': 'life style, diary',
+        'text': 'Papoy? Wha kind a papoy? No no no, paPOY! Ohhh, paPOY. He he he',
+        'title': 'My first blog'
+    }
+
     def setUp(self):
         factory.create_blog()
 
@@ -42,4 +61,14 @@ class TestPostStory(TestCase):
     def test_empty_form_is_not_valid(self):
         form = StoryForm() 
         self.assertFalse(form.is_valid())
+
+    def test_create_valid_story(self):
+        form = StoryForm(data=self.data)
+        self.assertTrue(form.is_valid(), form.errors)
+
+    def test_create_story_on_13th_jan(self):
+        data = self.data.copy()
+        data['date'] = '01/13/2013 00:16'
+        form = StoryForm(data=data)
+        self.assertTrue(form.is_valid(), form.errors)
 
