f59268d852c7188be6f922b0ba3b8dffd9b5cdf3
juacompe <juacompe@gmail.com>
2013-08-01 11:55:51 +0700

    - refactor test (BaseTestCase) - remove 13th Jan tests and use new year eve as default

--- alva/blogs/tests/test_forms.py
+++ alva/blogs/tests/test_forms.py
@@ -5,10 +5,10 @@ from blogs.models import Post, Story
 from blogs.tests import factory
 
 
-class TestPostForm(TestCase):
+class BaseTestCase(TestCase):
     data = {
         'blog': 1,
-        'date_0': '01/08/2013 09:20',
+        'date_0': '31/12/2013 00:16',
         'description': '',
         'dirty': True,
         'markup': 'markdown',
@@ -21,6 +21,8 @@ class TestPostForm(TestCase):
     def setUp(self):
         factory.create_blog()
 
+
+class TestPostForm(BaseTestCase):
     def test_post_form_is_for_post_model(self):
         self.assertEqual(Post, PostForm.Meta.model)
         
@@ -28,33 +30,12 @@ class TestPostForm(TestCase):
         form = PostForm() 
         self.assertFalse(form.is_valid())
 
-    def test_create_valid_post(self):
+    def test_create_valid_post_on_newyear_eve(self):
         form = PostForm(data=self.data)
         self.assertTrue(form.is_valid())
 
-    def test_create_post_on_13th_jan(self):
-        data = self.data.copy()
-        data['date_0'] = '13/01/2013 00:16'
-        form = PostForm(data=self.data)
-        self.assertTrue(form.is_valid(), form.errors)
 
-
-class TestPostStory(TestCase):
-    data = {
-        'blog': 1,
-        'date': '01/08/2013 09:20',
-        'description': '',
-        'dirty': True,
-        'markup': 'markdown',
-        'slug': 'my-first-blog',
-        'tags': 'life style, diary',
-        'text': 'Papoy? Wha kind a papoy? No no no, paPOY! Ohhh, paPOY. He he he',
-        'title': 'My first blog'
-    }
-
-    def setUp(self):
-        factory.create_blog()
-
+class TestPostStory(BaseTestCase):
     def test_story_form_is_for_story_model(self):
         self.assertEqual(Story, StoryForm.Meta.model)
         
@@ -62,13 +43,9 @@ class TestPostStory(TestCase):
         form = StoryForm() 
         self.assertFalse(form.is_valid())
 
-    def test_create_valid_story(self):
-        form = StoryForm(data=self.data)
-        self.assertTrue(form.is_valid(), form.errors)
-
-    def test_create_story_on_13th_jan(self):
+    def test_create_valid_story_on_newyear_eve(self):
         data = self.data.copy()
-        data['date'] = '01/13/2013 00:16'
+        data['date'] = '12/31/2013 00:16'
         form = StoryForm(data=data)
         self.assertTrue(form.is_valid(), form.errors)
 
