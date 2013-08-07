b919e26a87c8d7d05eaccff4937ddce91f0fa592
Roberto Alsina <ralsina@netmanagers.com.ar>
2013-08-07 16:08:45 -0300

    don't do that

--- alva/manage.py
+++ alva/manage.py
@@ -5,9 +5,9 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "alva.settings")
 
-    if 'migrat' not in ' '.join(sys.argv):
-        import alva.startup as startup
-        startup.run()
+    #if 'migrat' not in ' '.join(sys.argv):
+    #    import alva.startup as startup
+    #    startup.run()
 
     from django.core.management import execute_from_command_line
 
