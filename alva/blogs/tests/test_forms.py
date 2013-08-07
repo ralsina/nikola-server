f8d099c37b4242d6a9580f15f5dc87419d16c93d
Roberto Alsina <ralsina@netmanagers.com.ar>
2013-08-07 17:17:11 -0300

    refactor test (BaseTestCase)

--- alva/blogs/tests/test_forms.py
+++ alva/blogs/tests/test_forms.py
@@ -5,10 +5,24 @@ from blogs.models import Post, Story
 from blogs.tests import factory
 
 
-class TestPostForm(TestCase):
+class BaseTestCase(TestCase):
+    data = {
+        'blog': 1,
+        'date_0': '31/12/2013 00:16',
+        'description': '',
+        'dirty': True,
+        'markup': 'markdown',
+        'slug': 'my-first-blog',
+        'tags': 'life style, diary',
+        'text': 'Papoy? Wha kind a papoy? No no no, paPOY! Ohhh, paPOY. He he he',
+        'title': 'My first blog'
+    }
+
     def setUp(self):
         factory.create_blog()
 
+
+class TestPostForm(BaseTestCase):
     def test_post_form_is_for_post_model(self):
         self.assertEqual(Post, PostForm.Meta.model)
         
@@ -16,30 +30,22 @@ class TestPostForm(TestCase):
         form = PostForm() 
         self.assertFalse(form.is_valid())
 
-    def test_create_valid_post(self):
-        data = {
-            'blog': 1,
-            'date_0': '01/08/2013 09:20',
-            'description': '',
-            'dirty': True,
-            'markup': 'markdown',
-            'slug': 'my-first-blog',
-            'tags': 'life style, diary',
-            'text': 'Papoy? Wha kind a papoy? No no no, paPOY! Ohhh, paPOY. He he he',
-            'title': 'My first blog'
-        }
-        form = PostForm(data=data)
+    def test_create_valid_post_on_newyear_eve(self):
+        form = PostForm(data=self.data)
         self.assertTrue(form.is_valid())
 
 
-class TestPostStory(TestCase):
-    def setUp(self):
-        factory.create_blog()
-
+class TestPostStory(BaseTestCase):
     def test_story_form_is_for_story_model(self):
         self.assertEqual(Story, StoryForm.Meta.model)
         
     def test_empty_form_is_not_valid(self):
         form = StoryForm() 
         self.assertFalse(form.is_valid())
+
+    def test_create_valid_story_on_newyear_eve(self):
+        data = self.data.copy()
+        data['date'] = '12/31/2013 00:16'
+        form = StoryForm(data=data)
+        self.assertTrue(form.is_valid(), form.errors)
 
